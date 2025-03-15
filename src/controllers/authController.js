9c83c8488ac3844d075ee3ab9c2e71f69109df30
mary <maryamzienaldeeen@gmail.com>
2025-03-15 17:54:43 +0200

    AUTH CONTROLLES HASHING

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,45 +2,54 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+// Signup endpoint  | Registration
+//Ensured bcrypt.hash is used before saving passwords.
 exports.signup = async (req, res) => {
     try {
         const { username, email, password, role } = req.body;
         const existingUser = await User.findOne({ where: { email } });
-
+        // Check if user already exists
         if (existingUser) {
             return res.status(400).json({ message: 'Email already in use' });
         }
-
-        const newUser = await User.create({ username, email, password, role });
+        // Hash the password before savin
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // Create new user
+        const newUser = await User.create({ username, email,password: hashedPassword, role });
+        // Send response with the token
         res.status(201).json({ message: 'User registered successfully', user: newUser });
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
     }
 };
+//module.exports = { signup };
 
+// Login endpoint
 exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ where: { email } });
 
+        // Check if user exists
         if (!user) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
-
+        // Compare the entered password with the stored hashed password     
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ message: 'Invalid credentials' });
+            return res.status(400).json({ message: 'Invalid credentials, Wrong Password' });
         }
-
+        // Generate JWT token
         const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
+        // Send response with the token
         res.json({ token, user: { id: user.id, username: user.username, email: user.email, role: user.role } });
+
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
     }
 };
 
-
+//module.exports = { login };
 
 
 
