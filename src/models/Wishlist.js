f0ef7c88fd49633873b8c58e39ac0be9350580ea
mary <maryamzienaldeeen@gmail.com>
2025-03-18 13:14:13 +0200

    signin

--- src/models/Wishlist.js
+++
@@ -1,8 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/sequelize');
-
-const Wishlist = sequelize.define('Wishlist', {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true }
-});
-
-module.exports = Wishlist;
